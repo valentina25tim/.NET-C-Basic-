0fff7161fe26fec2739661566adbec0890777ac7
Valentina Tim <tina.tinko01@gmail.com>
2022-09-24 16:14:48 +0300

    Added =Read from File= (Dictionary | System.IO) *files from/to - look full path and change  for your LOCAL path

---
+++ ReadFromFile/MyHomeWork2.0/MyHomeWork2.0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MyHomeWork2._0.ReadFromFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string thisLinkFrrom = "C:\\C#project\\.NET C# Basic\\.NET-C-Basic-\\ReadFromFile\\MyHomeWork2.0\\MyHomeWork2.0\\ReadFromFile\\FileFrom.txt";
+
+            char n = '"';
+            string thisTextAdd = $"{n}Sherlock Holmes{n}, Sir Arthur Conan Doyle.";
+
+
+            string thisLinkAddDoctionaryTo = "C:\\C#project\\.NET C# Basic\\.NET-C-Basic-\\ReadFromFile\\MyHomeWork2.0\\MyHomeWork2.0\\ReadFromFile\\FileTo.txt";
+
+
+            WriteLinesToFile writeNewLines = new WriteLinesToFile(thisLinkFrrom, thisTextAdd);
+
+
+            ReadAllFromFile read = new ReadAllFromFile(thisLinkFrrom);
+
+            read.PrintText();
+
+            read.AddToDictionary();
+
+            read.AddDictionaryToNewFile(thisLinkAddDoctionaryTo);
+
+
+
+
+
+
+
+
+            //read.AddToHashSet();
+        }
+    }
+}
