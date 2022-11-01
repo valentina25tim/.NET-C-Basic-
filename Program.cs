dd08f8d1e6c98e9900ce52653d1b6764f905555e
ValentinaT <104255308+valentina25tim@users.noreply.github.com>
2022-11-01 17:40:50 +0200

    Add files via upload
    d

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ABP_Dal.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABP_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser parsing = new Parser();
+            parsing.PrintAtConsole();
+        }
+    }
+}
